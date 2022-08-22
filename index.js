8d6e4d99abf248d86aca797f7d46a0c0a5dd0a5a
Billy Mitchell <hello@billymitchell.design>
2022-08-22 16:16:56 -0400

    get fetching orders working

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 import fetch from 'node-fetch';
+import {} from 'dotenv/config'
 
 let inputData = {
     PO_Number: "(7400)-9-S1",
     Tracking_Number: "1Z4598130316369225",
     Shipping_Method: "UPS Ground",
     Ship_Date: "2019-10-20",
+    Product_ID: 905669,
+    Quantity: 1,
+    SKU: "Hidden Test Product "
 }
-
 
 /*Anything Inside A Parenthesis*/
 const regExp = /\(([^)]+)\)/;
@@ -23,7 +26,7 @@ let orderID = inputData.PO_Number.replace(`${storeID_Object[0]}-`, "").replace("
 const storeMapping = {
     7400: {
         URL: "https://fbla.mybrightsites.com/",
-        API_KEY: "Removed",
+        API_KEY: `${process.env.API_KEY_7400}`,
     },
     // 12345: {
     //     URL: "https://subdomain.mybrightsites.com",
@@ -31,35 +34,83 @@ const storeMapping = {
     // },
 }
    
+console.log(storeMapping[7400].API_KEY);
+
 
 let currentStoreURL = storeMapping[storeID].URL
 let currentAPI_KEY = storeMapping[storeID].API_KEY
 
 
-fetch(`${currentStoreURL}api/v2.3.0/orders/${orderID}/shipments?token=${currentAPI_KEY}`, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'cache',
-    credentials: 'same-origin',
-    headers: {
-        'content-type': 'application/json',
-        // authorization: 'Bearer 123abc456def'
-    },
-        body: {
-            "shipment": {
-              "tracking_number": `${inputData.Tracking_Number}`,
-              "send_shipping_confirmation": true,
-              "ship_date": `${inputData.Ship_Date}`,
-              "shipping_method": `${inputData.Shipping_Method}`,
-            }
-          }
-    })
-    .then(response => {
-        console.log(response)
-    })
-    .catch(err => {
-        console.log(err)
-})
+let shipmentData = [
+    {
+        "shipment": {
+            "tracking_number": `${inputData.Tracking_Number}`,
+            "send_shipping_confirmation": true,
+            "ship_date": inputData.Ship_Date,
+            "note": "Lorem ipsum",
+            "shipping_method": `${inputData.Shipping_Method}`,
+            "line_items": [
+              {
+                "id": inputData.Product_ID,
+                "quantity": inputData.Quantity
+              }
+            ]
+        }
+    }
+]
+    
+
+
+async function getOrder() {
+
+    let url = `${currentStoreURL}api/v2.3.0/orders/${orderID}?token=${currentAPI_KEY}`
+
+    let response = await fetch(url, {
+        method: 'GET',
+        redirect: 'follow',
+        headers: {
+            'Accept': 'application/json',
+            },
+        })
+
+    return response.json()
+    
+}
+
+const data = await getOrder()
+
+console.log(data.line_items)
+
+
+data.line_items.forEach(line_item   => {
+    // console.log(line_item.final_sku);
+    // console.log(line_item.id);
+    if (line_item.final_sku === inputData.SKU )
+    console.log(line_item.id);
+});
+
+
+
+
+
+// fetch(`${currentStoreURL}api/v2.3.0/orders/${orderID}/shipments?token=${currentAPI_KEY}`, {
+//     method: 'POST',
+//     redirect: 'follow',
+//     headers: {
+//         'content-type': 'application/json',
+//     },
+//     body: JSON.stringify(shipmentData)
+//     })
+//     .then(response => {
+//         console.log(response)
+//     })
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(err => {
+//         console.log(err)
+// })
+
 
 // output = [
 //   {
