8be56f89c819238df1c80a3da4e60f157e969dda
Billy Mitchell <hello@billymitchell.design>
2022-08-23 15:17:00 -0400

    working product ID collected

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import fetch from 'node-fetch';
 import {} from 'dotenv/config'
 
 let inputData = {
-    PO_Number: "(7400)-9-S1",
+    PO_Number: "(7400)-10-S1",
     Tracking_Number: "1Z4598130316369225",
     Shipping_Method: "UPS Ground",
     Ship_Date: "2019-10-20",
-    Product_ID: 905669,
+    Product_ID: "905669",
     Quantity: 1,
     SKU: "Hidden Test Product "
 }
@@ -34,30 +34,12 @@ const storeMapping = {
     // },
 }
    
-console.log(storeMapping[7400].API_KEY);
-
 
 let currentStoreURL = storeMapping[storeID].URL
 let currentAPI_KEY = storeMapping[storeID].API_KEY
 
 
-let shipmentData = [
-    {
-        "shipment": {
-            "tracking_number": `${inputData.Tracking_Number}`,
-            "send_shipping_confirmation": true,
-            "ship_date": inputData.Ship_Date,
-            "note": "Lorem ipsum",
-            "shipping_method": `${inputData.Shipping_Method}`,
-            "line_items": [
-              {
-                "id": inputData.Product_ID,
-                "quantity": inputData.Quantity
-              }
-            ]
-        }
-    }
-]
+
     
 
 
@@ -71,45 +53,75 @@ async function getOrder() {
         headers: {
             'Accept': 'application/json',
             },
+        }).catch(err => {
+            console.log(err)
         })
 
     return response.json()
     
 }
 
+// Get Order Data
 const data = await getOrder()
 
-console.log(data.line_items)
+//for each order item
 
+// initialize items
+let itemBeingShipped = ""
 
 data.line_items.forEach(line_item   => {
-    // console.log(line_item.final_sku);
-    // console.log(line_item.id);
-    if (line_item.final_sku === inputData.SKU )
-    console.log(line_item.id);
+
+        // console.log(line_item);
+    
+        // if the sku = input data sku 
+        if (line_item.final_sku === inputData.SKU ){
+    
+            // set items
+            itemBeingShipped = line_item.id
+
+            return itemBeingShipped
+        }
 });
 
 
 
+console.log(itemBeingShipped);
 
+async function updateOrder() {
 
-// fetch(`${currentStoreURL}api/v2.3.0/orders/${orderID}/shipments?token=${currentAPI_KEY}`, {
-//     method: 'POST',
-//     redirect: 'follow',
-//     headers: {
-//         'content-type': 'application/json',
-//     },
-//     body: JSON.stringify(shipmentData)
-//     })
-//     .then(response => {
-//         console.log(response)
-//     })
-//     .then(result => {
-//         console.log(result)
-//     })
-//     .catch(err => {
-//         console.log(err)
-// })
+    let url = `${currentStoreURL}api/v2.3.0/orders/${orderID}/shipments?token=${currentAPI_KEY}`
+
+    let response = await fetch(url, {
+        method: 'POST',
+        redirect: 'follow',
+        headers: {
+            // 'Accept': 'application/json',
+            },
+        body: {
+            "shipment": {
+                "tracking_number": inputData.Tracking_Number,
+                "send_shipping_confirmation": true,
+                "ship_date": inputData.Ship_Date,
+                "note": "Lorem ipsum",
+                "shipping_method": inputData.Shipping_Method,
+                "line_items": [
+                    {
+                    "id": itemBeingShipped,
+                    "quantity": inputData.Quantity
+                    }
+                ]
+            }
+        }
+        }).then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.log(err)
+        })    
+}
+
+await updateOrder()
+
 
 
 // output = [
